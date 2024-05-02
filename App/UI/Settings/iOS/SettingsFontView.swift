fa86cda289047d5c51b1c4b2d9d710dc29370e04
Denis Bryukhanov <3509501+siberianspot@users.noreply.github.com>
2024-05-03 05:31:45 +0700

    Update SettingsFontView.swift

--- App/UI/Settings/iOS/SettingsFontView.swift
+++ App/UI/Settings/iOS/SettingsFontView.swift
@@ -22,7 +22,7 @@ struct SettingsFontView: View {
 												 colorMap: preferences.colorMap)
 
 			PreferencesList {
-				PreferencesGroup(header: Text(.localize("SETTINGS_FONT", comment: "Settings menu label"))) {
+				PreferencesGroup(header: Text(String.localize("SETTINGS_FONT", comment: "Settings menu label"))) {
 					PreferencesPicker(selection: preferences.$fontName, label: EmptyView()) {
 						ForEach(sortedFonts, id: \.key) { key, value in
 							HStack(alignment: .center) {
@@ -30,7 +30,7 @@ struct SettingsFontView: View {
 									Image(systemName: .arrowDownCircle)
 										.font(.body.weight(.medium))
 										.foregroundColor(.accentColor)
-										.accessibility(label: Text(.localize("SETTINGS_FONT_NOT_INSTALLED", comment:"Font settings - font not installed")))
+										.accessibility(label: Text(String.localize("SETTINGS_FONT_NOT_INSTALLED", comment: "Font settings - font not installed")))
 								}
 
 								Text(key)
@@ -44,13 +44,13 @@ struct SettingsFontView: View {
 #if !targetEnvironment(macCatalyst)
 				PreferencesGroup {
 					Stepper(value: preferences.$fontSizePhone, in: 6...20, step: 1) {
-						Text(String(format: .localize("SETTINGS_FONT_SIZE", comment:"Font settings"), Int(preferences.fontSizePhone)))
+						Text(String(format: String.localize("SETTINGS_FONT_SIZE", comment: "Font settings"), Int(preferences.fontSizePhone)))
 					}
 				}
 #endif
 			}
 		}
-		.navigationBarTitle(.localize("SETTINGS_FONT", comment: "Settings menu label"))
+		.navigationBarTitle(String.localize("SETTINGS_FONT", comment: "Settings menu label"))
 	}
 
 }
